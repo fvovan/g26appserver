00d70eb8aa78212075bdbb46dc8fedc97e0daec4
fvovan <fvovan@gmail.com>
2025-03-25 17:23:42 +0400

    stupid math

--- src/main/java/com/vovsn/g26appserver/web/controller/DummyController.java
+++ src/main/java/com/vovsn/g26appserver/web/controller/DummyController.java
@@ -2,21 +2,20 @@ package com.vovsn.g26appserver.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.xml.bind.DatatypeConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
-import java.security.MessageDigest;
+import java.lang.management.ThreadMXBean;
+
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
-import com.sun.management.OperatingSystemMXBean;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
 
 
 @RestController
@@ -37,92 +36,38 @@ public class DummyController {
     @GetMapping(value = "/delay/{msec}")
     public String delay(@PathVariable int msec) throws NoSuchAlgorithmException {
         long timeStart = System.nanoTime();
-        OperatingSystemMXBean mxbean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        ThreadMXBean mxbean = ManagementFactory.getThreadMXBean();
 
-        long cpuTimeStart = mxbean.getProcessCpuTime();
+        if (!mxbean.isCurrentThreadCpuTimeSupported()) {
+            throw new RuntimeException("Thread time is not supported");
+        }
+        long cpuTimeStart = mxbean.getCurrentThreadCpuTime();
 
-        long milliSecCounter = 0;
+        long cpuThreadTimeCounter = 0;
         long cycles = 0;
 
         StringBuilder dummy = new StringBuilder();
 
         SecureRandom random = SecureRandom.getInstanceStrong();
 
-        while (milliSecCounter < msec) {
-            byte[] randomBytes = new byte[4096];
-            random.nextBytes(randomBytes);
+        double number = random.nextDouble();
 
-            MessageDigest  messageDigest =  MessageDigest.getInstance("MD5");
-            messageDigest.update(randomBytes);
+        while (cpuThreadTimeCounter < msec * 1_000_000L) {
+            number = number * sqrt(sin(number * cos(number)));
+            number = number / sqrt(cos(number + cycles + 1));
 
-            byte[] digest = messageDigest.digest();
-            String hash = DatatypeConverter.printHexBinary(digest);
+            dummy.append(number);
 
-            int substrStart = random.nextInt(28);
-            int substrLength = random.nextInt(2);
-            dummy.append(hash, substrStart, substrStart + 1 + substrLength);
-
-
-            milliSecCounter = (System.nanoTime() - timeStart) / 1_000_000;
+            cpuThreadTimeCounter = mxbean.getCurrentThreadCpuTime() - cpuTimeStart;
             cycles += 1;
         }
         long hashLength = dummy.length();
 
-        milliSecCounter = (System.nanoTime() - timeStart) / 1_000_000;
-
-        long cpuTime = (mxbean.getProcessCpuTime() - cpuTimeStart) / 1_000_000;
-
         long totalTime = (System.nanoTime() - timeStart) / 1_000_000;
 
         return "MD5 cycles over 1K-string: " + cycles + " \n"
-                + "StopWatch: predicted CPU time: " + milliSecCounter + ", timer total: " + totalTime + "\n"
-                + "Process: " + cpuTime + "\n"
+                + "Real timer total: " + totalTime + "\n"
+                + "Thread CPU : " + cpuThreadTimeCounter / 1_000_000 + "\n"
                 + hashLength + dummy.charAt((int)hashLength - 1) + "\n";
-    }
-
-    @GetMapping(value = "/delay-device/{msec}")
-    public String delayDevice(@PathVariable int msec) throws NoSuchAlgorithmException {
-        long timeStart = System.nanoTime();
-
-        long milliSecCounter = 0;
-        long cycles = 0;
-
-        StringBuilder dummy = new StringBuilder();
-
-        try (FileInputStream fis = new FileInputStream("/dev/urandom")) {
-
-            while (milliSecCounter < msec) {
-                byte[] randomBytes = new byte[4096];
-                fis.read(randomBytes);
-
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                byte[] md5Bytes = md.digest(randomBytes);
-
-                String md5Hex = bytesToHex(md5Bytes);
-
-                dummy.append(md5Hex, 25, 28);
-
-                milliSecCounter = (System.nanoTime() - timeStart) / 1_000_000;
-                cycles += 1;
-            }
-            long hashLength = dummy.length();
-
-            milliSecCounter = (System.nanoTime() - timeStart) / 1_000_000;
-
-
-            return "MD5 cycles over 1K-string: " + cycles + " \n"
-                    + "StopWatch: predicted CPU time: " + milliSecCounter + "\n"
-                    + hashLength + dummy.charAt((int) hashLength - 1) + "\n";
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
     }
 }
