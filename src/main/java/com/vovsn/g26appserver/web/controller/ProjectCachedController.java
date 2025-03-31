d9ec704d6a1c3ecdf745268821a6407773bc6c69
fvovan <fvovan@gmail.com>
2025-03-31 13:08:36 +0400

    explicit hikari pool setting

--- src/main/java/com/vovsn/g26appserver/web/controller/ProjectCachedController.java
+++ src/main/java/com/vovsn/g26appserver/web/controller/ProjectCachedController.java
@@ -4,9 +4,7 @@ import com.vovsn.g26appserver.persistance.model.Project;
 import com.vovsn.g26appserver.service.ProjectService;
 import com.vovsn.g26appserver.web.dto.ProjectDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
